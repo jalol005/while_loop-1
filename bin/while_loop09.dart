e243bea9e809e12b70d80d7dc127bad8e3ccccce
Mirjalol <jalolubaydullayev575@gmail.com>
2023-05-12 12:31:42 +0500

    09

--- bin/while_loop09.dart
+++ bin/while_loop09.dart
@@ -4,4 +4,20 @@
   
   
 */
-void main() {}
+int func(int a) {
+  int b = 0;
+  int c = 0;
+  while (c <= a) {
+    
+    if (c % 2 == 0) {
+     print(c) ;
+    }
+    c++;
+  }
+  return b;
+}
+
+void main() {
+  print(func(9));
+}
+
